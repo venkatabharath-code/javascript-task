5ea6a79b9b7a1bd60ddcb824ef84c2bfbbc4702d
paladugubharath7 <paladugubharath7@gmail.com>
2026-05-27 11:22:11 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// String + String = String Joining
+alert("2" + "2"); // 22
+
+// Number + Number = Addition
+alert(2 + 6); // 8
+
+// Joining Strings
+alert("venkata" + " " + "Bharath"); // venkata Bharath
+
+// Implicit Conversion
+alert("4" - "1"); // 3
+
+// + is used to join strings
+// -, *, / work with numbers
+
+// Explicit Conversion
+let num = Number("10");
+alert(num + 5); // 15
+
+// Anything from prompt is treated as string
+let age = prompt("Enter your age");
+
+console.log(age); // string value
+
+// Convert string into number
+age = Number(age);
+
+alert(age + 5); // 5 is added to the age
+
